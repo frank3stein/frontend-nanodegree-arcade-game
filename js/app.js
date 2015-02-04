9cb92246389a59661f8614a132d1cf31ccbe3035
frank3stein <martinezi@Emils-MacBook-Pro.local>
2015-02-04 14:24:13 +0100

    Player > Enemies >Collision need to be defined
    Background is drawn, but the game entities do not work.

--- js/app.js
+++ js/app.js
@@ -1,19 +1,71 @@
+// var enemyMechanics=[
+//         spawnLocation:[
+//         [-100,(2*101+(101/2))],
+//         [-100,(3*101+(101/2))],
+//         [-100,(4*101+(101/2))]
+//         ],
+//         spawn:function (count){
+
+//         }
+//         ,
+//         spawnPoints:function(){
+//             var count=Math.floor((Math.random() * 3) + 1);
+//             if (count==1){
+
+//             }
+//             else if(count==2){
+
+//             }
+//             else {
+//                 count=3;
+//             }
+//         },
+
+
+// ]
+
+var Constants=new function() {
+    this.Objects={
+        ENEMY:{
+            start_x:-150,
+            start_y:200,
+            // start_y:[100,150,200],
+            width:101,
+            height:171
+        },
+        PLAYER:{
+            start_x:100,
+            start_y:100,
+            width:100,
+            height:100
+        }
+    }
+}
+
+
 // Enemies our player must avoid
-var Enemy = function() {
+var Enemy = function(x,y) {
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
-    var obj={loc:loc};
-    obj.move=function(){
-        obj.loc++;
-    };
+
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
+    this.x=Constants.Objects.ENEMY.start_x;
+    this.y=Constants.Objects.ENEMY.start_y;
+    // [Math.floor(Math.random() * start_y.length)];
     this.sprite = 'images/enemy-bug.png';
+
 }
 
 // Update the enemy's position, required method for game
 // Parameter: dt, a time delta between ticks
 Enemy.prototype.update = function(dt) {
+    if (this.x<650) {
+        this.x+=200*dt;
+    }
+    else{
+        this.x=Constants.Objects.ENEMY.start_x;
+    }
 
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
@@ -28,8 +80,10 @@ Enemy.prototype.render = function() {
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
-var Player =function(){
+var Player =function(x,y){
     this.sprite="images/char-boy.png";
+    this.x=Constants.Objects.PLAYER.start_x;
+    this.y=Constants.Objects.PLAYER.start_y;
 }
     Player.prototype.update=function(dt){
 
@@ -48,10 +102,23 @@ var Player =function(){
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
 var allEnemies=[
-
+    // spawnLocation:[
+    //     [-100,(2*101+(101/2))],
+    //     [-100,(3*101+(101/2))],
+    //     [-100,(4*101+(101/2))]
+    // ]
 
 ];
 
+
+// Here player and enemy sublass delegates its failed lookups to
+var player= Object.create(Player.prototype);
+var enemy=Object.create(Enemy.prototype);
+
+for (i=0;allEnemies.length<6;i++){
+allEnemies.push(enemy);
+}
+console.log(allEnemies);
 
 
 // This listens for key presses and sends the keys to your
